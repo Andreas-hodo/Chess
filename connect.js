692a40335a3408bfb937d05fc43e2a0629900fb1
Andreas-hodo <119518809+Andreas-hodo@users.noreply.github.com>
2024-11-16 17:51:08 +0200

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,16 +1,18 @@
 const all_buttons = document.querySelectorAll(".forms_btns")  
 const login_link = document.getElementById("member_log")
 const signup_link = document.getElementById("member_sign")
+const login_btn =  document.getElementById("loginbtn")
+const signup_btn =  document.getElementById("signupbtn")
+const elem = document.getElementById("login_form")
 login_link.addEventListener("click",()=>{
- document.getElementById("login_form").style.zIndex = 1;
+ elem.style.zIndex = 1;
 })
 signup_link.addEventListener("click",()=>{
- document.getElementById("login_form").style.zIndex = 0;
+ elem.getElementById("login_form").style.zIndex = 0;
 })
 all_buttons.forEach(button =>{
     button.addEventListener("click",(e)=>{
      var change = e.target.classList[1] 
-     var elem = document.getElementById("login_form")
      if(change == "login"){
       elem.style.zIndex = 1;   
      }else if(change == "signup"){
@@ -18,8 +20,8 @@ all_buttons.forEach(button =>{
      }
      })
   })
-
-var names = [];
+signup_btn.addEventListener("click",()=>{
+ var names = [];
 users_data.get().then((snapshot) => {
   snapshot.forEach((doc) => {
    names.push(doc.data().name)
@@ -28,3 +30,5 @@ users_data.get().then((snapshot) => {
   alert("Something went wrong...")
 });
 console.log(names);
+})
+
