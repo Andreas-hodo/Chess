79405b5fcd5ff4fc0921ce6ecfdbd14fb5b70d02
Andreas-hodo <119518809+Andreas-hodo@users.noreply.github.com>
2025-11-19 00:35:49 +0200

    Add files via upload

---
+++ games/script/supabase.js
@@ -0,0 +1,5 @@
+const storage = supabase.createClient(
+    "https://wzkllmyazrkywlzqgfmy.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6a2xsbXlhenJreXdsenFnZm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMTM2NjMsImV4cCI6MjA3ODg4OTY2M30.llOBw1c9J04q9QXYzJ6Mohrb6rJ_JfmRw0QQ5UOqVqk"
+) 
+
