c8d386ebe3e62a0671d674281b94ba14fee569c1
Andreas-hodo <119518809+Andreas-hodo@users.noreply.github.com>
2025-11-20 21:44:33 +0200

    Delete games directory

--- games/script/app.js
+++
@@ -1,9 +0,0 @@
-let profilebtn = document.getElementById("profbtn")
-
-profilebtn.addEventListener("click",()=>{
-    let profpopup = document.getElementById("userprofile")
-    let btnpopup = document.getElementById("profilebtn_cont")
-    btnpopup.style.display = "none"
-    profpopup.style.display = "flex"   
-})
-console.log("ji")
